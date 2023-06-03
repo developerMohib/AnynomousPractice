75eb89664a4fec208e6beca4c5c14313d20b348b
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-04 00:04:00 +0600

    Anynomous Practic

---
+++ app.js
@@ -0,0 +1,16 @@
+// for loop practice
+// ja mon caice tai likci
+
+function Bangladesh(){
+    
+    for(let i = 0; i < 12; i++){
+        document.write(`I would love to do coding.<br>`);
+    }
+}
+function Personal(){
+    for(let i = 0; i < 12; i++){
+        
+        document.write(`You shouldnot have done it. <br>`);
+    }
+}
+
